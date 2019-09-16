47b7a3265c0b07dda3a59301258c65190089ebaa
Robert Zieliński <hello@robertz.co>
2019-09-17 04:04:33 +0900

    Add users to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.first_or_create(
-  email: 'hello@robertz.co',
-  password: '12345678'
-)
+emails = [
+  'hello@robertz.co',
+  'test1@robertz.co',
+  'test3@robertz.co',
+  'test4@robertz.co',
+  'test5@robertz.co',
+  'test6@robertz.co',
+  'test7@robertz.co',
+  'test8@robertz.co',
+  'test9@robertz.co',
+  'test10@robertz.co',
+  'test11@robertz.co',
+  'test12@robertz.co',
+]
+
+emails.each do |email|
+  next if User.find_by_email(email)
+  User.create(email: email, password: '12345678')
+  puts "New user with email (#{email}) created!"
+end
