c297db64060a9028cbad7e3ed5b5e7514f8c31bc
Robert Zieliński <hello@robertz.co>
2020-05-15 08:58:21 +0200

    Profile update endpoint

--- app/api/profile_api.rb
+++ app/api/profile_api.rb
@@ -12,5 +12,25 @@ class ProfileAPI < Grape::API
     get do
       { profile: current_user.as_json(only: %i[email username]) }
     end
+
+    desc 'Update' do
+      headers ApiDescHelper.with_common_headers
+    end
+    params do
+      requires :username, type: String, desc: 'Username'
+      requires :email, type: String, desc: 'User email'
+    end
+    post do
+      user = current_user
+      if user.update(declared(params))
+        status 200
+        { profile: user.as_json(only: %i[email username]) }
+      else
+        response_json = {
+          error_message: I18n.t('api.auth.sign_in.email_or_password_wrong')
+        }
+        error!(response_json, 401)
+      end
+    end
   end
 end
