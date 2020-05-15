38e703dabbec687f619a808b3e2453b46f3abcf6
Robert Zieliński <hello@robertz.co>
2020-05-15 08:58:21 +0200

    Profile update endpoint

--- spec/api/profile_api_spec.rb
+++ spec/api/profile_api_spec.rb
@@ -22,4 +22,47 @@ describe ProfileAPI do
       expect(json['profile']).to eq user.slice(:email, :username)
     end
   end
+
+  describe '#UPDATE' do
+    let(:user_attributes) do
+      { email: 'new@email.com', username: 'new_username' }
+    end
+
+    subject do
+      post '/api/profile', params: params.merge(user_attributes), headers: headers
+    end
+
+    include_context :should_check_valid_authentication_token
+
+    context 'succeeded' do
+      it 'should be success' do
+        subject
+        expect(response.status).to eq 200
+      end
+
+      it 'should return new profile data' do
+        subject
+
+        json = JSON.parse(response.body)
+        expect(json['profile']).to eq(user_attributes.stringify_keys)
+      end
+    end
+
+    context 'did not succeeded' do
+      before do
+        user_attributes[:username] = nil
+      end
+
+      it 'should be unauthorized' do
+        subject
+        expect(response.status).to eq 401
+      end
+
+      it 'should return error message' do
+        subject
+        json = JSON.parse(response.body)
+        expect(json['error_message']).not_to be_nil
+      end
+    end
+  end
 end
