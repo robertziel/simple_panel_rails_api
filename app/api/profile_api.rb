6f2db599ed7d36b04c275f6a2722d6eb72ac1f72
Robert Zieliński <hello@robertz.co>
2020-05-15 13:16:22 +0200

    Profile update endpoint allows to change password

--- app/api/profile_api.rb
+++ app/api/profile_api.rb
@@ -19,10 +19,22 @@ class ProfileAPI < Grape::API
     params do
       requires :username, type: String, desc: 'Username'
       requires :email, type: String, desc: 'User email'
+      optional :password, type: String, desc: 'New password'
+      optional :password_confirmation, type: String, desc: 'Confirm new password'
     end
     post do
       user = current_user
-      if user.update(declared(params))
+
+      user_attributes = declared(params)
+      # remove password from user_attributes if blank
+      if user_attributes[:password].blank?
+        user_attributes.except!(:password, :password_confirmation)
+      else
+        user_attributes[:password] = user_attributes[:password] || ''
+        user_attributes[:password_confirmation] = user_attributes[:password_confirmation] || ''
+      end
+
+      if user.update(user_attributes)
         status 200
         { profile: user.as_json(only: %i[email username]) }
       else
