a116e8b91e9b2ceee23add01c5ea6e3105083d70
Robert Zieliński <hello@robertz.co>
2020-05-22 22:54:25 +0200

    Active tokens expire endpoint

--- app/api/auth/active_tokens_api.rb
+++ app/api/auth/active_tokens_api.rb
@@ -4,20 +4,35 @@ module Auth
       authenticate!
     end
 
+    helpers do
+      def active_tokens
+        current_user.authentication_tokens.valid
+      end
+    end
+
     resource :active_tokens do
       desc 'Index' do
         headers ApiDescHelper.with_common_headers
       end
-
       get do
-        active_tokens = current_user.authentication_tokens.valid
-
         status 200
         {
           active_tokens: serialize_collection(
             active_tokens, serializer: Api::Auth::ActiveTokenSerializer
           )
         }
+      end
+
+      desc 'Expire tokens' do
+        headers ApiDescHelper.with_common_headers
+      end
+      params do
+        requires :ids, type: Array[Integer], desc: 'Remove authentication tokens'
+      end
+      post do
+        active_tokens.find(params[:ids]).each(&:expire!)
+        status 200
+        { success: true }
       end
     end
   end
--- spec/api/auth/active_tokens_api_spec.rb
+++ spec/api/auth/active_tokens_api_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe Auth::ActiveTokensAPI do
   let!(:user) { create(:user) }
-  let!(:active_token) { create(:authentication_token) }
+  let!(:user_2) { create(:user) }
+  let!(:active_token) { create(:authentication_token, user: user) }
+  let(:headers) { {} }
 
-  describe 'GET current_user' do
-    let(:headers) { {} }
-
+  describe 'GET index' do
     subject do
       get '/api/auth/active_tokens', headers: headers
     end
@@ -14,10 +14,6 @@ describe Auth::ActiveTokensAPI do
     include_context :should_check_valid_authentication_token
 
     context 'authentication_token belongs to user' do
-      before do
-        active_token.update!(user: user)
-      end
-
       it 'should be success' do
         subject
         expect(response.status).to eq 200
@@ -34,6 +30,10 @@ describe Auth::ActiveTokensAPI do
     end
 
     context 'authentication_token does not belong to user' do
+      before do
+        active_token.update!(user: user_2)
+      end
+
       it 'should be success' do
         subject
         expect(response.status).to eq 200
@@ -45,6 +45,42 @@ describe Auth::ActiveTokensAPI do
         expect(json[:active_tokens]).to eq serialize_collection(
           [authentication_token], serializer: Api::Auth::ActiveTokenSerializer
         )
+      end
+    end
+  end
+
+  describe 'POST Expire tokens' do
+    let(:params) { { ids: [active_token.id] } }
+
+    subject do
+      post '/api/auth/active_tokens', params: params, headers: headers
+    end
+
+    include_context :should_check_valid_authentication_token
+
+    context 'authentication_token belongs to user' do
+      it 'should be success' do
+        subject
+        expect(response.status).to eq 200
+      end
+
+      it 'expires authentication_token' do
+        expect { subject }.to change { user.authentication_tokens.valid.count }.by(-1)
+
+        json = response_body_to_json
+        expect(json[:success]).to be true
+      end
+    end
+
+    context 'authentication_token does not belong to user' do
+      before do
+        active_token.update!(user: user_2)
+      end
+
+      it 'does not expire any authentication_token' do
+        expect do
+          expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+        end.to change { AuthenticationToken.valid.count }.by(0)
       end
     end
   end
